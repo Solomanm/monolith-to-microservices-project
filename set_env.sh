bb46147c7bd9921de746dcd0d8ccf112ecf931c8
Solomanm <mtileni.proud@gmail.com>
2022-09-22 12:45:18 +0200

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=database1
+export POSTGRES_PASSWORD=Database1
+export POSTGRES_HOST=database1.c3h1fvhccq33.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::rmonoliths
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
