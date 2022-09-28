859cac5ba4c84db6c453a6ca8d190a52dc97159b
Solomanm <mtileni.proud@gmail.com>
2022-09-28 14:21:28 +0200

    My microservices project

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a0969e3cfa6f849ed9c96bb0afb03ce8-1113839628.us-east-1.elb.amazonaws.com'
 };
 
 /*
